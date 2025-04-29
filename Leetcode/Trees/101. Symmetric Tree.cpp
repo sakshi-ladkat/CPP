aafb6f5f4d889baf57c4d08410c3c674d3fef912
sakshi-ladkat <ladkatsakshi2507@gmail.com>
2025-04-29 16:43:31 +0530

    Leetcode Problem

--- Leetcode/Trees/101. Symmetric Tree.cpp
+++ Leetcode/Trees/101. Symmetric Tree.cpp
@@ -14,9 +14,10 @@ class Solution
 public:
     bool isSymmetric(TreeNode *root)
     {
-
         return isMirror(root->left, root->right);
     }
+
+private:
     bool isMirror(TreeNode *t1, TreeNode *t2)
     {
         if (!t1 && !t2)
@@ -24,6 +25,6 @@ public:
         if (!t1 || !t2 || t1->val != t2->val)
             return false;
 
-        return (isMirror(t1->left, t2->right), isMirror(t1->right, t2->left));
+        return isMirror(t1->left, t2->right) && isMirror(t1->right, t2->left);
     }
 };
