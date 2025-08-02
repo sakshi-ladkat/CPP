8d0a9edee25e792f7f89afaff07cd23ed280d9a7
Sakshi Ladkat <ladkatsakshi2507@gmail.com>
2025-08-02 23:28:03 +0530

    Update 942. DI String Match.cpp

--- Leetcode/Simple/942. DI String Match.cpp
+++ Leetcode/Simple/942. DI String Match.cpp
@@ -4,9 +4,9 @@ public:
         int sz=s.size();
         vector<int>ans;
         int ci=0,cd=sz;
-        for(int i=0;i<sz;i++)
+        for(int i=0;i<=sz;i++)
         {
-            if(ans.size()<sz)
+            if(ans.size()<=sz)
             {
             if(s[i]=='I' ) 
                ans.push_back(ci),ci++;
