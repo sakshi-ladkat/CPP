66cdd894d9655284c8d5bdf8ba52eb96c3bb3820
Sakshi Ladkat <ladkatsakshi2507@gmail.com>
2025-08-03 23:53:38 +0530

    Update 821. Shortest Distance to a Character.cpp

--- Leetcode/Simple/821. Shortest Distance to a Character.cpp
+++ Leetcode/Simple/821. Shortest Distance to a Character.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     vector<int> shortestToChar(string s, char c) {
         int sz = s.size();
-        vector<int>ans;
-        for(int i=0;i<sz;i++)
-        {
-            for(int j=i;j<sz;j++)
-            {
-                if(s[j]==c)
-                {
-                      ans.push_back(j-i);
-                      break;
+        vector<int> ans;
+
+        for (int i = 0; i < sz; i++) {
+            int minDist = sz;  
+            for (int j = 0; j < sz; j++) {
+                if (s[j] == c) {
+                    int dist = abs(j - i);
+                    minDist = min(minDist, dist);
                 }
-                
             }
+            ans.push_back(minDist);
         }
         return ans;
     }
 };
+
