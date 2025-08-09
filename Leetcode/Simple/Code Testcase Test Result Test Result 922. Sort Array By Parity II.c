2cb6508fe4c78edcb654196eb558350b5eaba55f
Sakshi Ladkat <ladkatsakshi2507@gmail.com>
2025-08-09 23:44:54 +0530

    Create  Code Testcase Test Result Test Result 922. Sort Array By Parity II.c

---
+++ Leetcode/Simple/Code Testcase Test Result Test Result 922. Sort Array By Parity II.c
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int size = nums.size();
+        for(int i=0;i<size;i++)
+        {
+            for(int j=0;j<size;j++)
+            {
+                if()
+            }
+        }
+        
+    }
+};
