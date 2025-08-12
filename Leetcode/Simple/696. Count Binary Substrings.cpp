aea7621d14fc6433528f0ce29baaae9eaa9b37af
Sakshi Ladkat <ladkatsakshi2507@gmail.com>
2025-08-12 23:50:44 +0530

    Create 696. Count Binary Substrings.cpp

---
+++ Leetcode/Simple/696. Count Binary Substrings.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int size = s.size();
+        int i=0;
+        int j=1;
+        while(i!=size)
+        {
+            while(j)
+        }
+        
+    }
+};
